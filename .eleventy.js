9a2b12c09debd5d933adebe9220b7184e96f7c75
John Seong <wonmor@gmail.com>
2022-05-12 19:35:02 -0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,5 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
-const markdownIt = require('markdown-it')
-const markdownItAttrs = require('markdown-it-attrs')
-
-const markdownItOptions = {
-    html: true,
-    breaks: true,
-    linkify: true
-}
-
-const markdownLib = markdownIt(markdownItOptions).use(markdownItAttrs)
-eleventyConfig.setLibrary('md', markdownLib)
-
 module.exports = function(eleventyConfig) {
     // Do NOT erase the two lines below...
     eleventyConfig.addPassthroughCopy("./static/images");
