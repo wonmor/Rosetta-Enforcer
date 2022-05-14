72c01c0a14cd5fc3cffa77968c79a3dbe819a113
John Seong <wonmor@gmail.com>
2022-05-14 11:57:00 -0400

    Official Website Build 2.0

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,23 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
+const pluginTOC = require('eleventy-plugin-toc')
+
+const markdownIt = require('markdown-it')
+const markdownItAnchor = require('markdown-it-anchor')
+
 module.exports = function(eleventyConfig) {
     // Do NOT erase the two lines below...
     eleventyConfig.addPassthroughCopy("./static/images");
     eleventyConfig.addPassthroughCopy("./**./**/*.{jpg,png,svg}");
+    eleventyConfig.addPassthroughCopy("./dist/output.css");
 
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
+    eleventyConfig.addPlugin(pluginTOC)
 
+    eleventyConfig.setLibrary(
+        'md',
+        markdownIt().use(markdownItAnchor)
+    )
 
     return {
         markdownTemplateEngine: "njk",
@@ -15,6 +26,7 @@ module.exports = function(eleventyConfig) {
             // default: [site root]
             input: "static",
             // default: _site
+            includes: "_includes",
             output: "_site",
         },
     };
