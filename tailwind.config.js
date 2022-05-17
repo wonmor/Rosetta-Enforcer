b6171b3d1e180a1c18ed92a3b38c37ed95e0a78b
John Seong <wonmor@gmail.com>
2022-05-16 21:11:28 -0400

    Recover files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+        "./_site/*.{html,js}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
+// To do: add a favicon file to ./images => convert the PNG file to ICO => code written on top of the base.njk file
